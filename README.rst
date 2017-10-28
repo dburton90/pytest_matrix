918c12973b89c9c6084e8f44e5c640d5cd2987ba
Daniel Barton <daniel.barton@seznam.cz>
2017-10-28 23:01:43 +0200

    initial

---
+++ README.rst
@@ -0,0 +1,15 @@
+Pytest Matrix
+^^^^^^^^^^^^
+
+This plugin provide simple way how to generate multiple test from combinations of setup data.
+
+
+Base usecase:
+=============
+
+.. code:: Python
+    from pytest_mixin import MatrixTestMixin
+    from myproject import divide_two_numbers
+
+
+    class TestDivision()
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(name="pytest_matrix",
+      version=0.1,
+      description='Provide tools for generating tests from combinations of fixtures.',
+      long_description='This plugin provide simple way how to generate multiple test from combinations of setup data.',
+      url="https://github.com/dburton90/pytest_mixin.git",
+      author="Daniel Bartoň",
+      author_email='daniel.barton@seznam.cz',
+      license='GPLv3',
+      packages=['pytest_mixin'],
+      install_requires=[
+          'pytest'
+      ],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Plugins',
+          'Framework :: Pytest',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Software Development :: Libraries',
+      ],
+      keywords='pytest mixin pytest_mixin generating tests',
+      zip_safe=False
+      )
